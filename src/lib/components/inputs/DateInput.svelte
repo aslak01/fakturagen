ba5afb1a2288a24fe790a153c058d0464dbb8b19
aslak <aslak.bakkeland@everyday.no>
2022-07-21 23:44:09 +0200

    fix: add date input

---
+++ src/lib/components/inputs/DateInput.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import LabelAsterisk from './LabelAsterisk.svelte'
+
+  export let label: string
+  export let required = false
+  export let value: Date
+  export let error: string | void
+</script>
+
+<label>
+  {label}<LabelAsterisk {required} />
+  <input
+    type="date"
+    {required}
+    aria-invalid={error ? 'true' : undefined}
+    bind:value
+  />
+  {#if error}
+    <small>{error}</small>
+  {/if}
+</label>
+
+<style>
+  small {
+    color: var(--form-element-invalid-active-border-color);
+  }
+</style>
