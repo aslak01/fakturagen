3a088260ed7b01732fff2f3c747e919a23749be9
aslak <aslak.bakkeland@everyday.no>
2022-07-22 16:55:11 +0200

    fix: number input

---
+++ src/lib/components/inputs/NumberInput.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import LabelAsterisk from './LabelAsterisk.svelte'
+
+  export let label: string
+  export let required = false
+  export let value: number
+  export let error: string | void
+</script>
+
+<label>
+  {label}<LabelAsterisk {required} />
+  <input
+    type="number"
+    {required}
+    aria-invalid={error ? 'true' : undefined}
+    bind:value
+  />
+  {#if error}
+    <small>{error}</small>
+  {/if}
+</label>
+
+<style>
+  small {
+    color: var(--form-element-invalid-active-border-color);
+  }
+</style>
