a2f1f20f8ff33bc49217ed5593da6362d26791f9
aslak <aslakrb@gmail.com>
2023-02-20 01:46:31 +0100

    add schema

---
+++ prisma/seed.ts
@@ -0,0 +1,108 @@
+import { PrismaClient } from '@prisma/client'
+import { aMonthInTheFuture } from '../src/lib/utils/index'
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.user.deleteMany()
+  await prisma.meta.deleteMany()
+  await prisma.address.deleteMany()
+  await prisma.company.deleteMany()
+  await prisma.bank.deleteMany()
+  await prisma.invoice.deleteMany()
+  await prisma.invoiceLine.deleteMany()
+
+  await prisma.user.create({
+    data: {
+      name: "Test Bruker",
+      company: {
+        create: {
+          name: "Test Firma",
+          currency: "NOK",
+          bank: {
+            create: {
+              accno: '123412341234',
+              iban: 'ibaniban',
+              bic: 'whatsabicagain',
+              name: "test bank"
+            }
+          },
+          address: {
+            create: {
+              street: "Some Street",
+              poNo: "1234",
+              city: "some city",
+              country: "Some country"
+            }
+          },
+          meta: {
+            create: {
+              locale: "nb-NO",
+              vatEnabled: true,
+              vatRate: 25
+            }
+          }
+        }
+      }
+    }
+  })
+
+  await prisma.company.create({
+    data: {
+      name: 'Customer',
+      currency: 'EUR',
+      address: {
+        create: {
+          street: 'Street',
+          poNo: '1233',
+          city: 'Citystan',
+          country: 'Countrynesia'
+        }
+      },
+      invoices: {
+        create: [
+          {
+            number: 1234,
+            date: new Date(),
+            dueDate: aMonthInTheFuture(),
+            paid: true,
+            sumPaid: 1234,
+            currValue: 3.14,
+            lines: {
+              create: [
+                {
+                  doneDate: new Date(),
+                  description: 'Some service',
+                  value: 3,
+                  currency: 'EUR',
+                  hasVat: true,
+                  vatAmt: 0.75
+                },
+                {
+                  doneDate: new Date(),
+                  description: 'Some other service',
+                  value: 0.14,
+                  currency: 'EUR',
+                  hasVat: true,
+                  vatAmt: 0.01
+                }
+              ]
+            }
+          }
+        ]
+      }
+
+    }
+  })
+
+
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
